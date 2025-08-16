dfaa973e24ba8a12d94d1c1b716120c8a8e77a28
smmasudrana <smmasudrana62@gmail.com>
2025-08-16 23:23:50 +0600

    problem number 2 solved

---
+++ problem2.js
@@ -0,0 +1,31 @@
+// Problem-02: Count Vowels
+
+// ⚠️ Function Name Must be countVowels()
+
+// একটা function বানাও, যেটা একটা string ইনপুট নিবে এবং ওই string এর মধ্যে কতগুলো vowel (a, e, i, o, u) আছে সেটা count করে return করবে।
+
+// 📥 Input:
+
+// "programming is fun"
+
+
+// 🚀 Output:
+
+// 5
+
+
+function countVowels(vowels){
+    const vowel=['a', 'e', 'i','o', 'u']
+    const allVowels=[];
+
+    for(const char of vowels){
+        if(vowel.includes(char)){
+            allVowels.push(char);
+        }
+    }
+    return allVowels;
+}
+
+const sentence=('programming is fun');
+const result =countVowels(sentence);
+console.log(result);
